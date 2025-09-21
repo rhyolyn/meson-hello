001acb86c59853e9c83744eb7697de83eed47564
rhyolyn <rhyolyn@gmail.com>
2025-09-20 17:32:04 -0700

    unit test stubs

---
+++ apps/hello/tests/test_hello.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+// Your test cases here
+TEST(HelloTest, Example) {
+    EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
